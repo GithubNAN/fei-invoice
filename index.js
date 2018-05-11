2224cbe836b3cd570ace4c1e6164429cc62c07ef
Johnson <xxx@xxx.com>
2018-05-11 16:20:57 +1000

    Refactor index.js, instead save worksheets into one workbook, save worksheet into its individual workbook

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const Excel = require("exceljs");
-const moment = require("moment");
-const _ = require("lodash");
-const path = require("path");
+import fs from "fs-extra";
+import Excel from "exceljs";
+import moment from "moment";
+import _ from "lodash";
+import path from "path";
+
+import config from "./history/config";
 
 // constant values use
 const GSTCELL = `E40`;
@@ -12,74 +15,69 @@ const INVOICECELL = `E4`;
 const DATESENTCELL = `E5`;
 const DATEPERIODCELL = `A18`;
 
-// Paths
-const xlsxReadOriginalPath = path.resolve(__dirname, "test/fei.xlsx");
-const xlsxReadFilePath = path.resolve(__dirname, "test/readTest001.xlsx");
-const xlsxWriteFilePath = path.resolve(__dirname, "test/writeTest001.xlsx");
-// File extracted
-// const xlsxInvoice = path.resolve(__dirname, "test/Invoice.xlsx");
-const xlsxInvoice = path.resolve(__dirname, "history/Invoice.xlsx");
+// folder path
+const invoiceFolder = path.resolve(__dirname, "history");
+// latest file path
+const latestInvoice = path.resolve(
+  __dirname,
+  "history",
+  `${config.latestInvoiceFileName}.xlsx`
+);
 
-// INPUT VALUES GOES HERE!!!
-const salary = 2282.5;
+// weekly wage input HERE!
+const wage = 1000;
 
-// Other setting
+// Moment time default setting
 moment.locale("en-AU");
 
 const workbook = new Excel.Workbook();
-workbook.xlsx.readFile(xlsxInvoice).then(function(wb) {
-  //Create new sheet using latest sheet name
+workbook.xlsx.readFile(latestInvoice).then(function(wb) {
+  // Copying latest sheet workbook sheet
   wb.properties.date1904 = true;
+  // Search the list of wb worksheet list
   const filterdWorksheet = _.filter(wb._worksheets);
-  //   console.log(filterdWorksheet)
-  const previouseWorksheet = wb.getWorksheet(
+  // Locate the worksheet
+  const latestWorksheet = wb.getWorksheet(
     filterdWorksheet[filterdWorksheet.length - 1].id
   );
-  const previousName = previouseWorksheet.name;
+  // Extract worksheet name
+  const previousName = latestWorksheet.name;
+  // Compose new worksheet name
   let week = parseInt(previousName.replace("W", ""));
   const newWorksheetName = `W${week + 1}`;
-  //   console.log(wb);
-  let sheet = wb.addWorksheet(newWorksheetName, {
-    views: [{ xSplit: 1, ySplit: 1 }]
-  });
 
-  // Create a new workbook
-  // const new_wb = new Excel.Workbook();
+  // Change config file
+  config.latestWeek = newWorksheetName;
+  config.latestInvoiceFileName = `Invoice Fei ${config.latestWeek}`;
+  // save config
+  // fs.outputJson(`./history/config.json`, config, err => console.log(err))
 
-  // new_wb.creator = "Fei";
-  // new_wb.lastModifiedBy = "Fei";
-  // new_wb.created = new Date();
-  // new_wb.modified = new Date();
-  // new_wb.lastPrinted = new Date();
+  // Create a new workbook and worksheet
+  const new_wb = new Excel.Workbook();
 
-  // new_wb.properties.date1904 = true;
-  // const new_sheet = workbook.addWorksheet(newWorksheetName);
+  new_wb.creator = "Fei";
+  new_wb.lastModifiedBy = "Fei";
+  new_wb.created = new Date();
+  new_wb.modified = new Date();
+  new_wb.lastPrinted = new Date();
 
-  // Debug
-  console.log(
-    `Previous worksheet id: ${previouseWorksheet.id}, name: ${
-      previouseWorksheet.name
-    }`
-  );
-  console.log(`current worksheet id: ${sheet.id}, name: ${sheet.name}`);
+  new_wb.properties.date1904 = true;
+  const new_sheet = new_wb.addWorksheet(newWorksheetName);
 
   // Retrive associated invoices number and date information from previous sheet
   // Invoice Number
-  const invoiceNumber = previouseWorksheet.getCell(INVOICECELL).value;
+  const invoiceNumber = latestWorksheet.getCell(INVOICECELL).value;
   const newInvoiceNumber = `FA${parseInt(invoiceNumber.replace("FA", "")) + 1}`;
 
   // Invoice sent date
-  const dateSent = previouseWorksheet.getCell(DATESENTCELL).value;
-  console.log(dateSent)
+  const dateSent = latestWorksheet.getCell(DATESENTCELL).value;
+  console.log(dateSent);
   const newDateSent = moment(dateSent, "DD-MMM-YY")
     .add(7, "days")
     .format("DD-MMM-YY");
 
-  //
-  //   console.log(dateSent);
-  //   console.log(newDateSent);
   // Time about invoice includes
-  const datePeriods = previouseWorksheet.getCell(DATEPERIODCELL).value;
+  const datePeriods = latestWorksheet.getCell(DATEPERIODCELL).value;
   const duration = datePeriods.split(" ~~ ");
   const startDay = duration[0];
   const endDay = duration[1];
@@ -91,32 +89,31 @@ workbook.xlsx.readFile(xlsxInvoice).then(function(wb) {
     .format("DD/MM/YYYY");
 
   //Copy information from previous sheet to newly created sheet
-  sheet["_columns"] = _.cloneDeep(previouseWorksheet["_columns"]);
-  sheet["_rows"] = _.cloneDeep(previouseWorksheet["_rows"]);
-  //   sheet["_columns"] = Object.assign({}, previouseWorksheet["_columns"]);
-  //   sheet["_rows"] = Object.assign({}, previouseWorksheet["_rows"]);
+  new_sheet["_columns"] = _.cloneDeep(latestWorksheet["_columns"]);
+  new_sheet["_rows"] = _.cloneDeep(latestWorksheet["_rows"]);
 
   // Change the values on the newly created sheet
-  sheet.getCell(TOTALCELL).value = salary;
-  sheet.getCell(GSTCELL).value = salary / 11;
-  sheet.getCell(AMOUNTCELL).value = salary - sheet.getCell(GSTCELL).value;
-  sheet.getCell(SUBTOTALCELL).value = salary - sheet.getCell(GSTCELL).value;
+  new_sheet.getCell(TOTALCELL).value = wage;
+  new_sheet.getCell(GSTCELL).value = wage / 11;
+  new_sheet.getCell(AMOUNTCELL).value = wage - new_sheet.getCell(GSTCELL).value;
+  new_sheet.getCell(SUBTOTALCELL).value =
+    wage - new_sheet.getCell(GSTCELL).value;
   // Change the invoice number
-  sheet.getCell(INVOICECELL).value = newInvoiceNumber;
+  new_sheet.getCell(INVOICECELL).value = newInvoiceNumber;
   // Change invoice sent date
-  sheet.getCell(DATESENTCELL).value = newDateSent;
+  new_sheet.getCell(DATESENTCELL).value = newDateSent;
   // Change invoice periods
-  sheet.getCell(DATEPERIODCELL).value = `${newStartDay} ~~ ${newEndDay}`;
+  new_sheet.getCell(DATEPERIODCELL).value = `${newStartDay} ~~ ${newEndDay}`;
 
-  // Save changed onto local file
-  wb.xlsx
-    .writeFile(xlsxInvoice)
-    .then(function(err) {
-      console.log(err);
+  new_wb.xlsx
+    .writeFile(
+      path.resolve(__dirname, "history", `${config.latestInvoiceFileName}.xlsx`)
+    )
+    .then(() => {
+        // save config
+        fs.outputJson(`./history/config.json`, config, err => console.log(err))
     })
     .catch(function(err) {
       console.log(err);
     });
-  // Save changes into new workbook file
-  // new_wb.writeFile;
 });
